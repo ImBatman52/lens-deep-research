e2d39ce83ba8bf8ff1fdf84bdfaf641cee95acd3
ImBatman52 <84771490+ImBatman52@users.noreply.github.com>
2025-02-09 09:48:11 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'batresearch'
+const title = 'BatResearch'
 const description =
   'A fully open-source AI-powered answer engine with a generative UI.'
 
